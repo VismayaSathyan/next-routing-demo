c2ab7ee08b23db06a7692367ebfc4586aad575e0
VismayaSathyan <vismayasathyan157@gmail.com>
2024-05-21 21:30:33 +0530

    dynamic routing with products folder ->[productId]folder-> the component taskes it as params

---
+++ src/app/products/page.tsx
@@ -0,0 +1,10 @@
+export default function Products() {
+  return (
+    <>
+      <h1>My Products</h1>
+      <h2 ><a href="../products/1">P1</a></h2>
+      <h2>P2</h2>
+      <h3>P3</h3>
+    </>
+  )
+}
