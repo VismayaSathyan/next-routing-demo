dd5dc6f5c48c6a0a1e3bdce65dc617554e627c9c
VismayaSathyan <vismayasathyan157@gmail.com>
2024-05-22 00:29:44 +0530

    custom not-found page and importing notFound() from next/navigation

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,15 @@
+import { notFound } from "next/navigation"
 export default function ReviewDetails({ params }
   : { params: { reviewId: string, productId: string } }) {
-  return <>
-    <h1>Reviews Detais</h1>
-    <h2>Product Id: {params.productId}</h2>
-    <h2>Review Id: {params.reviewId} </h2>
-  </>
+  if (parseInt(params.reviewId) > 1000) {
+    notFound();
+  } else {
+    return <>
+
+      <h1>Reviews Detais</h1>
+      <h2>Product Id: {params.productId}</h2>
+      <h2>Review Id: {params.reviewId} </h2>
+    </>
+
+  }
 }
