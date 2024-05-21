5d6150e3ed4f152565eef2fd07e3bf1344bce63a
VismayaSathyan <vismayasathyan157@gmail.com>
2024-05-22 00:15:39 +0530

    catch all segments

---
+++ src/app/docs/[[...slugs]]/page.tsx
@@ -0,0 +1,16 @@
+
+export default function Docs({ params, }:
+  {
+    params: {
+      slugs: string[]
+    };
+  }
+) {
+
+  if (params.slugs?.length === 2) {
+    return <h1>View feature {params.slugs[0]} and concept {params.slugs[1]}</h1>
+  } else if (params.slugs?.length === 1) {
+    return <h1>View feature {params.slugs[0]}</h1>
+  }
+  return <h1>Docs</h1>
+}
